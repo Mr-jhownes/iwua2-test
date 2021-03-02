4acf72815cc376496f4fb06135e30a51ffa6d402
Mr-jhownes <jhon_jonathan_cs@hotmail.com>
2021-03-02 10:24:51 +0000

    restar

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const http = require('http');
-const axios = require('axios');
+const axios = require('axios').default;
 
  http.createServer((req, res)=>{
-      res.write("Hello world \n"); // write a response
+      res.write(users.join(", ")); // write a response
+      
       res.end(); //end the response
     }).listen(8000); // listen for requests on port 8000
 
-    let users = []; // names of users will be stored here
-    axios.get("https://jsonplaceholder.typicode.com/users")
-    .then(({ data }) => {
-      users = data.map(user => user.name); // get only the names of the users and store in an array
-    })
-    .catch(error=>{
-      console.log(error);
-    });
+ let users = []; // names of users will be stored here
+ let email = [];
+ 
+(async function getNames(){
+  try{
+    const {data} = await axios.get("https://swapi.dev/api/people");
+    users = data.map(user=>user.name);
+    
+    console.log(users)
+  } catch(error){
+    console.log(error)
+  }
+})()
