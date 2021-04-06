ea0556f7246411813cc23111634b9fbbcb1ec32c
Mr-jhownes <jhon_jonathan_cs@hotmail.com>
2021-04-06 09:14:56 +0000

    image folder, multer

--- routes.js
+++ routes.js
@@ -16,4 +16,11 @@ router.get('/users', userCtrl.getUsers);
 router.get('/users/:id', userCtrl.getUser);
 router.put('/users/:id', userCtrl.updateUser);
 router.delete('/users/:id', userCtrl.deleteUser);
+
+    module.exports.UPLOAD_PATH = "uploads";
+
+    var multer = require('multer');
+    var upload = multer({ dest: module.exports.UPLOAD_PATH});
+    var imageCrtl = require('./image-controller');
+    
     module.exports = router;
